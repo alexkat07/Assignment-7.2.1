8d0f6862a01709a28f9ecbcc69ac210f35a89ad2
alexk <alexk@DESKTOP-QL7PNT4>
2024-06-17 16:48:50 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Assignment_7._2._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var vowelDict = new Dictionary<string, int> { { "a", 1 }, { "e", 2 }, { "i", 3 }, { "o", 4 }, { "u", 5 } };
+            
+
+            string s = "intelligent";
+
+            int l = 0;
+            int r = s.Length - 1;
+
+            while (l < r) 
+            {
+                if (vowelDict.ContainsKey(s[l].ToString()) && vowelDict.ContainsKey(s[r].ToString()))
+                {
+                    char temp = s[l];
+                    s = s.Remove(l, 1).Insert(l, s[r].ToString());
+                    s = s.Remove(r, 1).Insert(r, temp.ToString());
+                    l++;
+                    r--;
+
+
+                }
+                else if (vowelDict.ContainsKey(s[l].ToString()) && !vowelDict.ContainsKey(s[r].ToString()))
+                {
+                    r--;
+                }
+                else if (!vowelDict.ContainsKey(s[l].ToString()) && vowelDict.ContainsKey(s[r].ToString()))
+                {
+                    l++;
+                }
+                else 
+                {
+                    l++;
+                    r--;
+                }
+
+
+            }
+            Console.WriteLine(s);
+        }
+    }
+}
